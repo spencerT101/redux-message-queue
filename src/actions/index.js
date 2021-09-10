6a71a29ad2b4ac0c3e2bcf05b8c2a69160de38d2
Spencer <Spencer.Taber@yahoo.com>
2021-09-10 15:41:05 +0100

    action file written

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
 let newMessage = 1;
+
+export const addMessage = (message) => {
+    return{
+        type: "ADD_MESSAGE",
+        id: newMessage++,
+        message
+    };
+};
+
+export const removeMessage = id => {
+    return{
+        type: "REMOVE_MESSAGE"
+        payload: { id }
+
+    };
+};
+

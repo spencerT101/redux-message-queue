bd6c68776e2533489a01943b7e50f56e8abd374e
Spencer <Spencer.Taber@yahoo.com>
2021-09-14 11:42:54 +0100

    error handling added to handleNewMessage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {addMessage} from "./actions/messageActions";
+import { addMessage } from "./actions/messageActions";
 import MessageList from './components/messagelist';
 import { useState } from 'react';
 
@@ -8,32 +8,31 @@ import { useState } from 'react';
 function App() {
 
   const [message, setMessage] = useState('');
-   const handleNewMessage = (event) => {
 
-     event.preventDefault();
+  const handleNewMessage = (event) => {
+    if (!message.length) {
+      alert("Please write a message")
+      return;
+    } else {
+      event.preventDefault();
       addMessage(message)
     }
+  }
 
-
-return (
+  return (
     <div className="App">
       <h1>Message Impossible</h1>
-      <MessageList/>
-     
+      <MessageList />
 
-    
       <form >
-      <textarea
-            type="text"
-            name="message"
-            placeholder="message"
-            onChange = {event => setMessage(event.target.value)}
-            required
-          
+        <textarea
+          type="text"
+          name="message"
+          placeholder="message"
+          onChange={event => setMessage(event.target.value)}
           />
-          
-  
-     <button onClick={handleNewMessage} >Submit</button> 
+
+         <button onClick={handleNewMessage} >Submit</button>
       </form>
     </div>
   );

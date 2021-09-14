02dd805f6837a9aba7dc8d15604fd3abe8e1f958
Spencer <Spencer.Taber@yahoo.com>
2021-09-14 19:27:37 +0100

    adjusted styling on form and messageitem components

--- src/components/messageForm.js
+++ src/components/messageForm.js
@@ -4,34 +4,35 @@ import "./messageForm.css"
 import Button from '@material-ui/core/Button';
 
 
-const MessageForm = function () { 
-  
-  const [message, updateMessage] = useState('');
+const MessageForm = function () {
 
-const handleNewMessage = (event) => {
-  if (!message.length) {
-    alert("Did you think it would be that easy agent? Write a message to complete your mission!")
-    return;
-  } else {
-    event.preventDefault();
-    setMessage(message)
-  }
+    const [message, updateMessage] = useState('');
+
+    const handleNewMessage = (event) => {
+        if (!message.length) {
+            alert("Did you think it would be that easy agent? Write a message to complete your mission!")
+            return;
+        } else {
+            event.preventDefault();
+            setMessage(message)
+        }
+    }
+
+    return (
+        <div>
+            <form>
+                <input
+                    alt="message box"
+                    type="text"
+                    name="message"
+                    placeholder="message"
+                    onChange={event => updateMessage(event.target.value)}
+                    id="text-box"
+                />
+                <Button variant="contained" onClick={handleNewMessage} id="button">Submit</Button>
+            </form>
+        </div>
+    )
 }
-
-return (
-    <div>
-        <form>
-            <input
-                alt="message box"
-                type="text"
-                name="message"
-                placeholder="message"
-                onChange={event => updateMessage(event.target.value)}
-                id = "text-box"
-            />
-            <Button variant="contained" onClick={handleNewMessage} id = "button">Submit</Button>
-        </form>
-    </div>  
-)}
 
 export default MessageForm;
